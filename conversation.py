e9f23d15309717d062d99b1abfb07b868820352e
Kaki <kaki.so2011@gmail.com>
2018-09-10 14:19:02 -0400

    Conversation

---
+++ conversation.py
@@ -0,0 +1,15 @@
+#September 7th, 2018
+#This program will ask you basic questions and answer in specific ways utilizing the answer that you have given.
+#source: none
+
+print("oh, hey there. I see you.")
+name = input("What's your name?")
+print("Hi, "+name+"!")
+mood = input ("How are you?")
+print("What a coincidence! I am feeling "+mood+" too.")
+subject = input("What's your favorite subject by the way?")
+print("I think "+subject+" is cool too, but I like lunch the best.")
+food = input("speaking of lunch, do you have a favorite food?")
+print("eww, "+food+" is kinda gross to me. My favorite is chips. Get it?")
+sleep= input("When do you go to bed?")
+print("ok, you got some time until "+sleep+". But I will sleep now. Computers get tired easily. Good night.")
