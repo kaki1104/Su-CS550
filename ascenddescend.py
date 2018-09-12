6c478047c61886ae36d3323835e31d6b392dd4a7
Kaki <kaki.so2011@gmail.com>
2018-09-12 15:59:38 -0400

    Windspeed Calculation

---
+++ ascenddescend.py
@@ -0,0 +1,12 @@
+
+# This program will tell you wehther the three values entered in the command line is either strictly ascending or decending.
+#Sources: http://thomas-cokelaer.info/tutorials/python/boolean.html
+#		  https://linuxconfig.org/python-boolean-operators
+
+import sys 
+x = sys.argv[1] 
+y = sys.argv [2]
+z = sys.argv [3]
+
+truth = (float(x)<float(y)<float(z)) or (float(x)>float(y)>float(z))
+print (truth)
---
+++ randomnumber.py
@@ -0,0 +1,9 @@
+
+import random 
+import math
+
+theta = random.random()*math.pi*2
+print (theta)
+
+trigresult = math.sin(theta)**2 + math.cos(theta)**2
+print (trigresult)
