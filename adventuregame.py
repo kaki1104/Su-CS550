decd961ef56913d7c2ec9526ef32567d45006467
Kaki <kaki.so2011@gmail.com>
2018-09-23 23:22:54 -0400

    sep 23

--- adventuregame.py
+++ adventuregame.py
@@ -1,6 +1,6 @@
 #Kaki Su
 #September 17, 2018
-#Description: 
+#Description: In this game, you will make a salad or a sandwich to satisfy your hunger level, which is assigned randomly in the beginning of the game. Your choices in the ingredients will affect how much satisfaction you will get, and your goal is to not be too full, but also not too hungry. While making food, the user must also perform certain tasks such as cutting food through pressing the keyboard quickly. 
 #sources: 
 #how to measure time used to perform a function: https://stackoverflow.com/questions/7370801/measure-time-elapsed-in-python
 #how to make the variables global: https://stackoverflow.com/questions/11904981/local-variable-referenced-before-assignment
@@ -17,29 +17,30 @@ import sys
 
 hungerlevel = random.randint (3,10)
 
-def clear():
+def clear(): #function to clear the terminal 
 	os.system('clear')
 
-def delay_print(s):
+
+def delay_print(s): #function that pauses a little between each letter so that it makes the sentences look like it's being typed out.
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.03)
 
-def hunger () :
+def hunger () : #explanation of hunger level system
 	global hungerlevel
 	hungerlevel = str(hungerlevel)
 	input ("Your current hunger level is "+hungerlevel+" [Press Enter]")
 	input ("Your goal is to decrease your hunger level to around 0. [Press Enter]")
-	input ("Anything from -2 to 2 will make you happy, but being too full or too hungry will make you unhappy. [Press Enter]")
+	input ("You will be happy if you end up with a hunger level between -2 and 2, but being too full or too hungry will make you unhappy. [Press Enter]")
 
-def start () :
+def start () : 
 	delay_print ("You are hungry......")
 	hunger()
 	clear ()
 	delay_print ("Now you want to make some food for yourself......")
 
-def cutting () :
+def cutting () : #the cutting task, used for both salad and sandwich
 	clear ()
 	from timeit import default_timer as timer
 	input ("Now you have to cut them by hitting c on the keyboard five times and hit enter. Make sure to do it fast.\n Are you ready?[Press Enter]")
@@ -47,7 +48,7 @@ def cutting () :
 	cut = input ("Go! ")
 	end = timer ()
 	if cut == "ccccc" :
-		if float(end - start) <= 3 :
+		if float(end - start) <= 3 : #if the task was performed under 3 seconds
 			input ("Okay, good. let's move on. [Press Enter]")
 			clear ()
 		else :
@@ -59,12 +60,7 @@ def cutting () :
 		 clear ()
 		 cutting()
 
-def showhunger () :
-	global hungerlevel
-	hungerlevel = str (hungerlevel)
-	input ("Your hunger level is "+hungerlevel+" now. [Press Enter]")
-
-def saladbase ():
+def saladbase (): 
 	clear ()
 	delay_print ("\nWhat greens do you want as your salad base?")
 	greens = input ("\nIt looks like you have lettuce, spinach, kale, and arugula in the fridge. All of these will decrease your hunger level by -2. \n Enter a salad base: ")
@@ -72,12 +68,12 @@ def saladbase ():
 		delay_print ("taking out "+greens+" from the fridge.........")
 		cutting ()
 		global hungerlevel 
-		hungerlevel = int (hungerlevel) - 2
+		hungerlevel = int (hungerlevel) - 2 #decreases hunger level by three
 	else :
-		delay_print ("I don't think it's a good idea...")
+		delay_print ("Are you sure you entered the right words? Try again..........")
 		saladbase ()
 
-def meatchoice () :
+def meatchoice () : #the choice of meet, works for both sandwich and salad
 	clear ()
 	delay_print ("I think we need some protein......   ")
 	meat = input ("\nDo you want chicken, pork, or beef? These will all decrease your hunger level by 3. \n Enter chicken, pork, beef, or I am vegetarian: ")
@@ -93,8 +89,8 @@ def meatchoice () :
 		input ("Please enter a valid command. [Press enter]")
 		meatchoice ()
 
-def toppingchoice() :
-	try: 
+def toppingchoice() : #made a seperate function from topping() because it was easier to put recursions in it.
+	try:
 		top1, top2 = input ("You can choose two toppings. \n Enter two toppings and split them by a space: ").split()
 		if (top1 == "tomato" or top1== "cucumber" or top1 == "avocado" or top1 =="onion" or top1 == "carrot" or top1 == "tofu") and (top2 == "tomato" or top2== "cucumber" or top2 == "avocado" or top2 =="onion" or top2 == "carrot" or top2 == "tofu") :
 			if top1 == top2 :
@@ -112,7 +108,7 @@ def toppingchoice() :
 		delay_print ("Are you sure you typed the right words?")
 		toppingchoice ()
 
-def topping () :
+def topping () : #choices of toppings, works for both salad and sandwich
 	clear ()
 	top = input ("Do you want toppings? \nType yes or no: ")
 	if top == "yes" : 
@@ -125,7 +121,7 @@ def topping () :
 		delay_print ("uh...what? Enter yes or no.")
 		toppingchoice()
 
-def sandwichbase () :
+def sandwichbase () : 
 	clear ()
 	input ("Let's choose what type of bread to use. [Press enter]")
 	bread = input ("What type of bread do you want to use for your sandwich? It looks like you have whole grain, whole wheat, multi grain, and rye. All of these will decrease your hunger level by -5. \nEnter a bread type: ")
@@ -133,10 +129,15 @@ def sandwichbase () :
 		delay_print ("taking out "+bread+" bread..................")
 		cutting ()
 		global hungerlevel 
-		hungerlevel = int (hungerlevel) - 5
+		hungerlevel = int (hungerlevel) - 5 #decreases hunger level by 5, but not shown to the user
 	else :
-		delay_print ("I don't think it's a good idea.........")
+		delay_print ("Are you sure you typed in the right words? Try again.........")
 		sandwichbase ()
+
+def showhunger () : #function that shows your hunger level in the end
+	global hungerlevel
+	hungerlevel = str (hungerlevel)
+	input ("Your hunger level is "+hungerlevel+" now. [Press Enter]")
 
 def salad() :
 	clear ()
@@ -160,7 +161,7 @@ def system () :
 	delay_print ("What do you want to make?")
 
 	loop = 1
-	while loop == 1:
+	while loop == 1: #will make the user try again until they enter either salad or sandwich
 		recipe = input (" \nYou can make a salad (decreases hunger level by 2-7) or a sandwich (decreases hunger level by 5-10). \n  Enter salad or sandwich: ")
 
 		if str.lower(recipe) == "salad":
@@ -179,16 +180,16 @@ def system () :
 	clear ()
 	showhunger ()
 	clear ()
-	if  -2 <= int (hungerlevel) <= 2 :
+	if  -2 <= int (hungerlevel) <= 2 : #if the hunger level is in the right range
 		delay_print ("You have successfully satisfied your hunger! Congrats!")
 		delay_print ("\n      _.-'''''-._\n    .'  _     _  '.\n   /   (o)   (o)   \ \n   |               |\n   |  \         /  |\n    '.  `'---'`  .'\n      '-._____.-'                            The End.")
-	elif -2 >= int (hungerlevel) :
-		delay_print ("Oops... I think you ate too much. You're feeling kinda gross. Be careful next time!")
-	elif 2 <= int (hungerlevel) :
+	elif -2 >= int (hungerlevel) : #if the hunger level is too low=too full
+		delay_print ("Oops... I think you ate too much. You're feeling kinda gross. Be careful next time!\n")
+	elif 2 <= int (hungerlevel) : #if the hunger level is still too high =still hungry
 		delay_print("Oops... I think you ate too little. You're still hungry...")
 		tryagain = input ("\n Do you want to try again? \n Enter yes or no:")
 		if str.lower(tryagain) == "yes" :
-			input ("I like your guts! Let's play again!")
+			input ("I like your guts! Let's play again! [Press enter to continue]")
 			system ()
 		elif str.lower(tryagain) == "no":
 			input ("Oh well. I will see you next time.")
