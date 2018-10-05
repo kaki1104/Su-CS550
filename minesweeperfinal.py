7b2bf85f0ac23ce1b1e2a244d1f5dbe9fdadcb2c
Kaki <kaki.so2011@gmail.com>
2018-10-04 23:13:56 -0400

    oct4

--- minesweeperfinal.py
+++ minesweeperfinal.py
@@ -7,8 +7,8 @@ import os
 def clear(): #function to clear the terminal 
 	os.system('clear')
 
-w = int (sys.argv [1])+2
-h = int (sys.argv [2])+2
+w = int (sys.argv [1]) + 2
+h = int (sys.argv [2]) + 2
 b = int (sys.argv [3])
 
 if b >= w * h :
@@ -22,7 +22,7 @@ loop = 0
 while loop < b :
 	x = random.randint (1, w-1)
 	y = random.randint (1, h-1)
-	l[y][x] = str("*")
+	l[x][y] = str("*")
 	loop = loop + 1
 
 for y in range (1,w-1) :
@@ -45,43 +45,53 @@ for y in range (1,w-1) :
 			if l[x-1][y-1] != "*" :
 				l[x-1][y-1] = int(l[x-1][y-1]) + 1
 
-def showboard () :
+def showm () :
 	clear ()
 	for y in range (1,w-1) :
 		for x in range (1,h-1) :
 			print (m[x][y],end=" ")
 		print("")
 
+def showl() :
+	for y in range (1,w-1) :
+		for x in range (1,h-1) :
+			print (l[x][y],end=" ")
+		print("")
+
 def action() :
 	try:
 		for y in range (1,w-1) :
 			for x in range (1,h-1) :
-				revealflag, x, y = input ("You can choose a space to either reveal or flag. \nEnter the position and whetehr you would like to reveal or flag, split by two spaces (ex. flag 1 1): ").split()
-				x = int (x)
-				y = int (y)
-				if revealflag == "flag" :
-					m[x][y] =  str("F")
-					showboard()
-				elif revealflag == "reveal" : 
-					if l[x][y] != "*":
-						m[x][y] = l[x][y]
-						if m [x][y] == 0 :
-							m[x-1][y-1] = l[x-1][y-1]
-							m[x-1][y+1] = l[x-1][y+1]
-							m[x-1][y] = l[x-1][y]
-							m[x][y-1] = l[x][y-1]
-							m[x][y+1] = l[x][y+1]
-							m[x+1][y-1] = l[x+1][y-1]
-							m[x+1][y] = l[x+1][y]
-							m[x+1][y+1] = l[x+1][y+1]
-						showboard()
-					elif l[x][y] == "*" :
-						print ("You've hit the bomb! Game over!")
-						quit()
-				else: 
-					print ("You must enter reveal or flag.")
+				if 
+			revealflag, x, y = input ("You can choose a space to either reveal or flag. \nEnter the position and whetehr you would like to reveal or flag, split by two spaces (ex. flag 1 1, reveal 2 3): ").split()
+			x = int (x)
+			y = int (y)
+			if revealflag == "flag" :
+				m[x][y] =  str("*")
+				showm()
+				action ()
+			elif revealflag == "reveal" :
+				if l[x][y] != "*":
+					m[x][y] = l[x][y]
+					if m [x][y] == 0 :
+						m[x-1][y-1] = l[x-1][y-1]
+						m[x-1][y+1] = l[x-1][y+1]
+						m[x-1][y] = l[x-1][y]
+						m[x][y-1] = l[x][y-1]
+						m[x][y+1] = l[x][y+1]
+						m[x+1][y-1] = l[x+1][y-1]
+						m[x+1][y] = l[x+1][y]
+						m[x+1][y+1] = l[x+1][y+1]
+					showm()
 					action()
-					
+				elif l[x][y] == "*" :
+					print ("You've hit the bomb! Game over!")
+					quit()
+			else: 
+				print ("You must enter reveal or flag.")
+				action()
+		print ("Congrats! You beat the game!")
+		quit ()
 	except ValueError :
 		print ("Are you sure you entered the command the right way?")
 		action ()
@@ -89,13 +99,6 @@ def action() :
 		print ("Are you sure your position was in the right range?")
 		action ()
 
-showboard()
-action()
-
-for y in range (1,w-1) :
-	for x in range (1,h-1) :
-		print (l[x][y],end=" ")
-	print("")
-
-
+showm ()
+action ()
 
