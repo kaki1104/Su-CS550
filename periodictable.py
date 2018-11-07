3655c9b11cc75cfcea8a17832e3444b71240707e
Kaki <kaki.so2011@gmail.com>
2018-11-06 20:52:35 -0500

    1106

--- periodictable.py
+++ periodictable.py
@@ -1,4 +1,5 @@
 #periodictable.py
+#https://stackoverflow.com/questions/3944655/testing-user-input-against-a-list-in-python
 
 class Element :
 	
@@ -14,12 +15,23 @@ class Element :
 	__repr__ = __str__
 
 
-
 def main () :
 	file = open('elements.csv')
 	allelements=[]
 	for line in file:
 		part = line.split(',')
 		allelements.append(Element(part[0],part[1],part[2],part[3]))
-	print (allelements)
+	while True : 
+		info = input ("\nEnter an element name, number, symbol, or weight: ")
+		for e in allelements : 
+			if info == e.element :
+				print (allelements[2])
+			elif e.number == info:
+				print (allelements[int(info)])
+			#elif e.symbol == info:
+				#print ("success!")
+			#elif e.weight == info:
+				#print ("success!")
+
+
 main ()
